667cd6868dd1b486e1cb04e522c70e1793a868ac
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-01 01:36:56 +0200

    Add  new composition code to test manifest actor.

--- AkkaTransfer/Program.cs
+++ AkkaTransfer/Program.cs
@@ -5,6 +5,7 @@ using Akka.Util.Internal;
 using AkkaTransfer;
 using AkkaTransfer.Actors;
 using AkkaTransfer.Data;
+using AkkaTransfer.Data.Manifest;
 using AkkaTransfer.Data.ReceiveFile;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,33 +18,61 @@ if (dbContext.Database.GetPendingMigrations().ToList().Count != 0)
     dbContext.Database.Migrate();
 }
 
-IReceiveFileHeaderRepository repo = new ReceiveFileHeaderRepository(dbContext);
+// Setup ManifestActor
 
-// Setup Actors.
+IManifestRepository receiveManifestRepo = new ReceiveManifestRepository(dbContext);
+IManifestRepository sendManifestRepo = new SendManifestRepository(dbContext);
 
 FileBox fileSendBox = new("SendBox");
 FileBox fileReceiveBox = new("ReceiveBox");
 
+ManifestHelper sendManifestHelper = new ManifestHelper(fileSendBox, sendManifestRepo);
+ManifestHelper receiveManifestHelper = new ManifestHelper(fileReceiveBox, receiveManifestRepo);
+
 Config hocon = HoconLoader.FromFile("akka.net.hocon");
 ActorSystem system = ActorSystem.Create("file-transfer-system", hocon);
 
-Props sendProps = Props.Create(typeof(SendFileActor), fileSendBox);
-IActorRef sendFileActor = system.ActorOf(sendProps, "send-file-actor");
+Props props = Props.Create<ManifestActor>(() => new ManifestActor(HoconLoader.ReadSendIpAndPort("hocon.send"), sendManifestHelper, receiveManifestHelper));
+IActorRef sendFileActor = system.ActorOf(props, "manifest-actor");
 
-Props rebuilderProps = Props.Create(typeof(FileRebuilderActor), fileSendBox, repo);
-_ = system.ActorOf(rebuilderProps, "file-rebuilder-actor");
-
-Props receiveGatewayProps = Props.Create(typeof(ReceiveFileActor), fileReceiveBox, repo)
-    .WithRouter(new SmallestMailboxPool(5, new DefaultResizer(5, 1000), SupervisorStrategy.DefaultStrategy, "default-dispatcher"));
-IActorRef receiveGateway = system.ActorOf(receiveGatewayProps, "receive-file-actor");
-
-// Send files.
-
-fileSendBox.GetFilesInBox()
-    .Select(filePath => Path.GetFileName(filePath))
-    .ForEach(sendFileActor.Tell);
-
+// TODO: This doesn't work because async, make a delegate to let you know when receiving files is done.
 while (true)
 {
-    Thread.Sleep(1000);
+    Console.WriteLine();
+    Console.WriteLine("Type \"receive\" to request file transfer.");
+
+    var input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "receive":
+            sendFileActor.Tell(new SendManifestRequest());
+            break;
+    }
 }
+
+//////IReceiveFileHeaderRepository repo = new ReceiveFileHeaderRepository(dbContext);
+
+//////// Setup Actors.
+
+//////FileBox fileSendBox = new("SendBox");
+//////FileBox fileReceiveBox = new("ReceiveBox");
+
+//////Config hocon = HoconLoader.FromFile("akka.net.hocon");
+//////ActorSystem system = ActorSystem.Create("file-transfer-system", hocon);
+
+//////Props sendProps = Props.Create(typeof(SendFileActor), fileSendBox);
+//////IActorRef sendFileActor = system.ActorOf(sendProps, "send-file-actor");
+
+//////Props rebuilderProps = Props.Create(typeof(FileRebuilderActor), fileSendBox, repo);
+//////_ = system.ActorOf(rebuilderProps, "file-rebuilder-actor");
+
+//////Props receiveGatewayProps = Props.Create(typeof(ReceiveFileActor), fileReceiveBox, repo)
+//////    .WithRouter(new SmallestMailboxPool(5, new DefaultResizer(5, 1000), SupervisorStrategy.DefaultStrategy, "default-dispatcher"));
+//////IActorRef receiveGateway = system.ActorOf(receiveGatewayProps, "receive-file-actor");
+
+//////// Send files.
+
+//////fileSendBox.GetFilesInBox()
+//////    .Select(filePath => Path.GetFileName(filePath))
+//////    .ForEach(sendFileActor.Tell);
