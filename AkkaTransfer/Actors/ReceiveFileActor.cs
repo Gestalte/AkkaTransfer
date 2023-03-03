886d3a990891b69ed77c744911cb8353dd5bd72d
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-03 20:51:28 +0200

    Move ReceiveFileCoordinator  to its  own  file.

--- AkkaTransfer/Actors/ReceiveFileActor.cs
+++ AkkaTransfer/Actors/ReceiveFileActor.cs
@@ -1,28 +1,9 @@
 ﻿using Akka.Actor;
-using Akka.Routing;
 using AkkaTransfer.Common;
 using AkkaTransfer.Data.ReceiveFile;
 
 namespace AkkaTransfer.Actors
 {
-    public class ReceiveFileCoordinatorActor : ReceiveActor
-    {
-        private readonly IReceiveFileHeaderRepository fileHeaderRepository;
-
-        public readonly FileBox box;
-
-        public ReceiveFileCoordinatorActor(FileBox box, IReceiveFileHeaderRepository fileHeaderRepository)
-        {
-            this.box = box;
-            this.fileHeaderRepository = fileHeaderRepository;
-
-            Props props = Props.Create(() => new ReceiveFileActor(this.box, this.fileHeaderRepository))
-                .WithRouter(new RoundRobinPool(5, new DefaultResizer(5, 1000)));
-
-            var receiveRouter = Context.ActorOf(props);
-        }
-    }
-
     public class ReceiveFileActor : ReceiveActor
     {
         private readonly IReceiveFileHeaderRepository fileHeaderRepository;
@@ -32,27 +13,24 @@ namespace AkkaTransfer.Actors
         public ReceiveFileActor(FileBox box, IReceiveFileHeaderRepository fileHeaderRepository)
         {
             this.fileHeaderRepository = fileHeaderRepository;
-
             this.box = box;
 
-            Receive<FilePartMessage>(Handle);
-        }
-
-        private void Handle(FilePartMessage message)
-        {
-            System.Diagnostics.Debug.WriteLine($"Receive part {message.Position} of {message.Count}");
-            Console.WriteLine($"Receive part {message.Position} of {message.Count}");
-
-            var id = this.fileHeaderRepository.AddNewPieceUnitOfWork(message);
-
-            var transactionActor = Context.ActorSelection("user/file-rebuilder-actor");
-
-            if (id == -1)
+            Receive<FilePartMessage>(message =>
             {
-                return;
-            }
+                System.Diagnostics.Debug.WriteLine($"Receive part {message.Position} of {message.Count}");
+                Console.WriteLine($"Receive part {message.Position} of {message.Count}");
 
-            transactionActor.Tell(id);
+                var id = this.fileHeaderRepository.AddNewPieceUnitOfWork(message);
+
+                var transactionActor = Context.ActorSelection("user/file-rebuilder-actor");
+
+                if (id == -1)
+                {
+                    return;
+                }
+
+                transactionActor.Tell(id);
+            });
         }
     }
 }
