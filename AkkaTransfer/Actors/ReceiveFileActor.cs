34fe719400fc6a2a032d9aef7796394d97e2f7b9
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-04 09:44:04 +0200

    ReceiveFileActor now uses the full address for file-rebuilder-actor.

--- AkkaTransfer/Actors/ReceiveFileActor.cs
+++ AkkaTransfer/Actors/ReceiveFileActor.cs
@@ -15,18 +15,18 @@ namespace AkkaTransfer.Actors
 
             Receive<FilePartMessage>(message =>
             {
-                ReceiveFileHeaderRepository fileHeaderRepository = new ReceiveFileHeaderRepository(new DbContextFactory());
+                ReceiveFileHeaderRepository fileHeaderRepository = new(new DbContextFactory());
 
                 System.Diagnostics.Debug.WriteLine($"Receive part {message.Position} of {message.Count}");
 
                 var id = fileHeaderRepository.AddNewPieceUnitOfWork(message);
 
-                var transactionActor = Context.ActorSelection("user/file-rebuilder-actor");
-
                 if (id == -1)
                 {
                     return;
                 }
+
+                var transactionActor = Context.ActorSelection("akka://file-transfer-system/user/file-rebuilder-actor");
 
                 transactionActor.Tell(id);
             });
