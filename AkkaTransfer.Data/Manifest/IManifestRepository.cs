3e9d1a5202aa94cd4df1379164d5c4c24f9748d7
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-05 02:25:46 +0200

    Move IManifestRepository to its own file.

---
+++ AkkaTransfer.Data/Manifest/IManifestRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace AkkaTransfer.Data.Manifest
+{
+    public interface IManifestRepository
+    {
+        Common.Manifest LoadNewestManifest();
+        void Save(Common.Manifest manifest);
+    }
+}
